c62180b998f8c0d605a3b532a45b64113ce994ce
Harlan <405086805@qq.com>
2016-11-11 10:41:53 +0800

    初次提交

---
+++ app/src/main/java/com/example/jerei/videodemo/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.jerei.videodemo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
+import android.media.MediaRecorder;
+import android.media.ThumbnailUtils;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by zhush on 2016/11/11
+ * E-mail zhush@jerei.com
+ * PS  視頻DEMO
+ */
+public class MainActivity extends AppCompatActivity implements VideoInputDialog.VideoCall{
+
+    ImageView image;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = (ImageView) findViewById(R.id.image);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                        //显示视频录制控件
+                        VideoInputDialog.show(getSupportFragmentManager(),MainActivity.this);
+            }
+        });
+    }
+
+
+    @Override
+    public void videoPathCall(String path) {
+
+        Log.e("地址:",path);
+        //根据视频地址获取缩略图
+        Bitmap bitmap = ThumbnailUtils.createVideoThumbnail(path, MediaStore.Video.Thumbnails.MINI_KIND);
+        image.setImageBitmap(bitmap);
+    }
+}
